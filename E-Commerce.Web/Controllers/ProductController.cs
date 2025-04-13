ba36c6b176907a262ed4ad54bd349a6f835afaa4
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-13 06:36:36 +0200

    Part 02 APIs Types

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using E_Commerce.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    [Route("api/[controller]")] //BaseURL/api/Product
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET : BaseURL/api/Product/10
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetAction(int id)
+        {
+            return new Product() { Id = id };
+        }
+        //BaseURL/api/Product
+        [HttpGet]
+        public ActionResult<Product> GetAll()
+        {
+            return new Product();
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            return new Product();
+        }
+        [HttpPut]
+        public ActionResult<Product> UpdateProduct(Product product)
+        {
+            return new Product();
+        }
+        [HttpDelete]
+        public ActionResult<Product> DeleteProduct(Product product)
+        {
+            return new Product();
+        }
+    }
+}
