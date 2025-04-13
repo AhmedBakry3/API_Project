b4c41e4557420b6c6f364115f1a4d54ebf1001b4
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-13 17:47:59 +0200

    Part 05 Product configurations and Dbcontext

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -11,7 +14,11 @@ namespace E_Commerce.Web
             #region Add services to the container
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(); 
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoredDbContext>(Options =>
+            {
+                Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             #endregion
 
             var app = builder.Build();
