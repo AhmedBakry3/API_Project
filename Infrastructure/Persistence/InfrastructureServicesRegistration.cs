5d51bcbda4554004b74a6c1139df9f8f1e0fa127
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-26 18:23:03 +0300

    Part 07 Refactor Program Class

---
+++ Infrastructure/Persistence/InfrastructureServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿namespace Persistence
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection Services , IConfiguration Configuration)
+        {
+            Services.AddDbContext<StoredDbContext>(Options =>
+            {
+                Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            Services.AddScoped<IDataSeeding, DataSeeding>();
+            Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return Services;
+        }
+    }
+}
